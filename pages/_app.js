c83d4496a007864ff10ad219432cae6c8c385d6a
Akshat Tiwari <tiwariakshat03@gmail.com>
2022-10-03 15:48:26 +0530

    Configuration updated
    - All the required pages / client side  endpoints have been added to the app.
    - Material UI also added to the app.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
-import Head from "next/head";
+import * as React from 'react';
+import Head from 'next/head';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider } from '@emotion/react';
+import theme from '../config/theme';
+import createEmotionCache from '../config/createEmotionCache';
+
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
 
 function MyApp({ Component, pageProps }) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
   return(
       <>
         <Head>
@@ -87,7 +98,16 @@ function MyApp({ Component, pageProps }) {
 
           <title>PetMe</title>
         </Head>
-        <Component {...pageProps} />
+        <CacheProvider value={emotionCache}>
+          <Head>
+            <meta name="viewport" content="initial-scale=1, width=device-width" />
+          </Head>
+          <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </CacheProvider>
       </>
   )
 }
