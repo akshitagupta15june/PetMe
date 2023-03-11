dfbe38d68a97b886defaea060bfbac4156176f82
ramonbs <ramongiovanii12@gmail.com>
2023-03-11 10:34:23 -0300

    refactor: improve responsiveness on donateform for mobile

--- src/components/Contributors/Contributors.test.jsx
+++ src/components/Contributors/Contributors.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitFor } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { vi } from 'vitest';
 import Contributors from '.';
