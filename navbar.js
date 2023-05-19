a4062a9fce5325b6cfece896668b8482c2e4aedf
Saksham Awasthi <sakshamawasthi1111@gmail.com>
2023-05-20 02:26:09 +0530

    Added the button feature
    Made a button feature in index.html
    Added CSS to index.css with the same id
    and added a js function in navbar.js

--- navbar.js
+++ navbar.js
@@ -2,6 +2,7 @@ const navBar = document.getElementById("navbar");
 const logo = document.getElementById("logo");
 const navBtn = document.getElementById("nav-button");
 
+
 let prevScrollPos =  window.pageYOffset
 
 navBtn.addEventListener("click", () => {
@@ -9,11 +10,29 @@ navBtn.addEventListener("click", () => {
   logo.classList.toggle("hidden");
 });
 
+document.getElementById('search-form').addEventListener('submit', function(event) {
+	event.preventDefault(); // Prevents form submission
+	
+	const searchQuery = document.getElementById('search-input').value;
+	const pageText = document.body.innerText;
+	
+	if (pageText.includes(searchQuery)) {
+	  // Navigate to the search results
+	  window.find(searchQuery);
+	} else {
+	  // Text not found
+	  alert('Text not found!');
+	}
+  });
+  
+  
+
 window.onscroll = function() {
 	myFunction()
 	let currentScrollPos =  window.pageYOffset
 	toggleNavBar(currentScrollPos > prevScrollPos, currentScrollPos)
 };
+
 
 var header = document.getElementById("header");
 // var sticky = header.offsetTop;
