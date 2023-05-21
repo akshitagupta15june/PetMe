d5351cdf941c76f633673c759fa6699f4f1be2c2
Arjun Dey <arjun.dey.2612@gmail.com>
2023-05-21 12:55:29 +0530

    Updated

---
+++ Gallery.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("img").click(function(){
+    var t = $(this).attr("src");
+    $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
+    $("#myModal").modal();
+  });
+  
+  $("video").click(function(){
+    var v = $("video > source");
+    var t = v.attr("src");
+    $(".modal-body").html("<video class='model-vid' controls><source src='"+t+"' type='video/mp4'></source></video>");
+    $("#myModal").modal();  
+  });
+  });//EOF Document.ready
