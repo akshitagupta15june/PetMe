f582b20c5d64b2a89a9a68beccb8648e35491c0f
Hemant2335 <knrt73373@gmail.com>
2023-05-29 18:20:25 +0530

    news update

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,7 @@ module.exports = {
       fontFamily: {
         sans: ["Graphik", "sans-serif"],
         serif: ["Merriweather", "serif"],
+        
       },
       extend: {
         spacing: {
