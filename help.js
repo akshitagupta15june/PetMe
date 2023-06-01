3b7dfb6f844e66ae45e6285f20a65392aacb5048
Rahul Phoolbhati <rahulphulbhati@gmail.com>
2023-06-01 14:54:17 +0530

    hep page added

---
+++ help.js
@@ -0,0 +1,8 @@
+
+
+const Gotopage = () =>{
+    var userInput = document.getElementById("input").value;
+    var link = "https://www.justdial.com/"+userInput+"/Pet-Clinics/nct-10616595";
+    window.location.href = link;
+}
+
