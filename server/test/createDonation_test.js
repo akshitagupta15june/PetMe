6e87a028553f6983d70f7a83b09e7a5bfe30aa18
Ethan Sue <ethans2028@gmail.com>
2024-01-23 15:45:47 -0800

    renamed users to donations

--- server/test/createDonation_test.js
+++ server/test/createDonation_test.js
@@ -7,7 +7,7 @@ describe('Creating Donation in MongoDB', () => {
         const newDonation = new Donation({ name: 'Shriyam' }); 
         newDonation.save() // returns a promise after some time 
             .then(() => { 
-                //if the newUser is saved in db and it is not new 
+                //if the newDonation is saved in db and it is not new 
                 assert(!newDonation.isNew); 
                 done(); 
             }); 
