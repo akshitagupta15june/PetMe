2671fa3c46736a61d44e72446f635add713c56b6
Ethan Sue <ethans2028@gmail.com>
2024-01-23 13:29:44 -0800

    created first test. installed mocha library to test backend api

---
+++ server/test/createDonation_test.js
@@ -0,0 +1,15 @@
+//import the User model 
+const Donation = require('../Models/donationModel');  
+const assert = require('assert'); 
+  
+describe('Creating Donation in MongoDB', () => { 
+    it('Creates a New Donation', (done) => { 
+        const newDonation = new Donation({ name: 'Shriyam' }); 
+        newDonation.save() // returns a promise after some time 
+            .then(() => { 
+                //if the newUser is saved in db and it is not new 
+                assert(!newDonation.isNew); 
+                done(); 
+            }); 
+    }); 
+});
